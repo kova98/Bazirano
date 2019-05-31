d2bf786780b9fb19437d8daecc09757049db48e2
Roko <rokokovac98@gmail.com>
2019-05-31 14:45:19 +0200

    Set up MVC

--- Bazirano/Startup.cs
+++ Bazirano/Startup.cs
@@ -11,13 +11,11 @@ namespace Bazirano
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,10 +23,9 @@ namespace Bazirano
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
         }
     }
 }
