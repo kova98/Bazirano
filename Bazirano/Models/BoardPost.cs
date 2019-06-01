a5144122b396486a0448b1206bb5eecdd637f473
Roko <rokokovac98@gmail.com>
2019-06-01 10:33:06 +0200

    Add BoardPost and BoardThread models

---
+++ Bazirano/Models/BoardPost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazirano.Models
+{
+    public class BoardPost
+    {
+        public long Id { get; set; }
+        public string Image { get; set; }
+        public DateTime DatePosted { get; set; }
+
+        [Required(ErrorMessage = "Molimo unesite tekst.")]
+        [MaxLength(500, ErrorMessage = "Tekst je predugačak.")]
+        [MinLength(10, ErrorMessage = "Tekst je prekratak.")]
+        public string Text { get; set; }
+    }
+}
---
+++ Bazirano/Models/BoardThread.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazirano.Models
+{
+    public class BoardThread
+    {
+        public long Id { get; set; }
+        public bool IsLocked { get; set; } = false;
+        public int PostCount { get; set; }
+        public int ImageCount { get; set; }
+        public ICollection<BoardPost> Posts { get; set; }
+    }
+}
