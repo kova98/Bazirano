c8d5e6adc365db238c241fe262572c940ff146bc
Roko <rokokovac98@gmail.com>
2019-06-09 10:18:34 +0200

    Add post response list

---
+++ Bazirano/Components/PostResponsesViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Bazirano.Models;
+using Bazirano.Models.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazirano.Components
+{
+    public class PostResponsesViewComponent : ViewComponent
+    {
+        private IBoardThreadsRepository repository;
+
+        public PostResponsesViewComponent(IBoardThreadsRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke(BoardPost post)
+        {
+            BoardThread thread = repository.BoardThreads.FirstOrDefault(t => t.Posts.Contains(post));
+
+            var responses = thread.Posts
+                .Where(p => p.Text.Contains($"#{post.Id}"))
+                .Select(x => x.Id);
+
+            return View(responses);
+        }
+    }
+}
