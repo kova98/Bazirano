043143b2caad0f163bfd60ecaa3f6285122c3e0b
Roko <rokokovac98@gmail.com>
2019-06-10 11:31:27 +0200

    Reorganize Models

--- Bazirano/Models/Board/BoardRespondViewModel.cs
+++ Bazirano/Models/Board/BoardRespondViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Bazirano.Models
+namespace Bazirano.Board.Models
 {
     public class BoardRespondViewModel
     {
