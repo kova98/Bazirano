b4adab8c8c011af74ae23996712ffd15ea7d4fd5
Roko <rokokovac98@gmail.com>
2019-06-10 11:34:08 +0200

    Revert "Reorganize Models"
    This reverts commit 043143b2caad0f163bfd60ecaa3f6285122c3e0b.

--- Bazirano/Models/BoardPost.cs
+++ Bazirano/Models/BoardPost.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Bazirano.Board.Models
+namespace Bazirano.Models
 {
     public class BoardPost
     {
