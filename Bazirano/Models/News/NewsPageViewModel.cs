a7481f463f60a5c35fba7bff0eb5d36d941052ed
Roko <rokokovac98@gmail.com>
2019-06-11 12:46:00 +0200

    Add News page partial views

---
+++ Bazirano/Models/News/NewsPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazirano.Models.News
+{
+    public class NewsPageViewModel
+    {
+        public NewsPost MainPost { get; set; }
+        public List<NewsPost> MainPostRelatedPosts { get; set; }
+
+        public NewsPost SecondaryPost { get; set; }
+        public List<NewsPost> PostList { get; set; }
+
+        public List<NewsPost> LatestNews { get; set; }
+    }
+}
