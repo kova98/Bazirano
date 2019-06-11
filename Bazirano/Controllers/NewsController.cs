038e0ddb62552ac2104a05b40b5dbf7533bccaf1
Roko <rokokovac98@gmail.com>
2019-06-11 15:03:30 +0200

    Add API for posting NewsPosts

--- Bazirano/Controllers/NewsController.cs
+++ Bazirano/Controllers/NewsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Bazirano.Infrastructure;
+using Bazirano.Models.DataAccess;
+using Bazirano.Models.News;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bazirano.Controllers
@@ -10,15 +12,36 @@ namespace Bazirano.Controllers
     public class NewsController : Controller
     {
         private INewsHelper helper;
+        private INewsPostsRepository repository;
 
-        public NewsController(INewsHelper helper)
+        public NewsController(INewsHelper helper, INewsPostsRepository repo)
         {
             this.helper = helper;
+            repository = repo;
         }
 
         public IActionResult Index()
         {
             return View(helper.CurrentNews);
+        }
+
+        //TODO: Add security!!!
+        [HttpPost("~/api/postNews")]
+        public IActionResult PostNews([FromBody]NewsPost post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.DatePosted = DateTime.Now;
+                post.Comments = new List<NewsComment>();
+
+                repository.AddNewsPost(post);
+
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
