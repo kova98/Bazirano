deb99a4ceb69e2cf84b779a8ba919976bc656f41
Roko <rokokovac98@gmail.com>
2019-06-18 10:33:01 +0200

    Add initial Home page design

--- Bazirano/Controllers/HomeController.cs
+++ Bazirano/Controllers/HomeController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Bazirano.Models.DataAccess;
+using Bazirano.Models.Home;
+using Bazirano.Infrastructure;
 
 namespace Bazirano.Controllers
 {
     public class HomeController : Controller
     {
+        private IBoardThreadsRepository boardRepo;
+        private INewsHelper newsHelper;
+
+        public HomeController(IBoardThreadsRepository boardRepo, INewsHelper newsHelper)
+        {
+            this.boardRepo = boardRepo;
+            this.newsHelper = newsHelper;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(new HomePageViewModel
+            {
+                MainPost = newsHelper.CurrentNews.MainPost,
+                PopularPosts = newsHelper.CurrentNews.PostList,
+                Threads = boardRepo.BoardThreads.Take(6).ToList()
+            });
         }
     }
 }
---
+++ Bazirano/Models/Home/HomePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Bazirano.Models.News;
+using Bazirano.Models.Board;
+using System.Collections.Generic;
+
+namespace Bazirano.Models.Home
+{
+    public class HomePageViewModel
+    {
+        public NewsPost MainPost { get; set; }
+        public List<NewsPost> PopularPosts { get; set; }
+        public List<BoardThread> Threads { get; set; }
+    }
+}
