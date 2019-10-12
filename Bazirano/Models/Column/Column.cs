926b2f295413aa36760a8a20e2639420c0f641fd
Roko <rokokovac98@gmail.com>
2019-10-12 15:22:37 +0600

    Add Column page

---
+++ Bazirano/Models/Column/Column.cs
@@ -0,0 +1,15 @@
+﻿using Bazirano.Models.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazirano.Models.Column
+{
+    public class Column
+    {
+        public string Author { get; set; }
+        public DateTime DatePosted { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
