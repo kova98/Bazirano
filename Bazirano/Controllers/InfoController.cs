3e110bde21ac800db4d61fa46307b1a3faac34c9
Roko <rokokovac98@gmail.com>
2020-03-05 10:08:56 +0100

    Add footer
    Adds a footer with social media, about and contact links with placeholder views

---
+++ Bazirano/Controllers/InfoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bazirano.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult About()
+        {
+            return View(nameof(About));
+        }
+
+        public IActionResult Contact()
+        {
+            return View(nameof(Contact));
+        }
+    }
+}
