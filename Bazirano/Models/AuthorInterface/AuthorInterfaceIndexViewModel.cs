2204ffec5a28906198f2f0d2ddb5c68ba2cc008a
Roko <rokokovac98@gmail.com>
2020-03-21 08:30:55 +0100

    Add Author interface index

---
+++ Bazirano/Models/AuthorInterface/AuthorInterfaceIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Bazirano.Models.Column;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazirano.Models.AuthorInterface
+{
+    public class AuthorInterfaceIndexViewModel
+    {
+        public Author Author { get; set; }
+        public List<ColumnRequest> DraftRequests { get; set; }
+        public List<ColumnRequest> PendingRequests { get; set; }
+        public List<ColumnRequest> RejectedRequests { get; set; }
+        public List<ColumnRequest> ApprovedRequests { get; set; }
+    }
+}
