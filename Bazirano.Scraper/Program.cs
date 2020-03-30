af140990db4f95ea4c4fffd31ecde88e41838627
Roko <rokokovac98@gmail.com>
2020-03-30 14:47:31 +0100

    Add the Bazirano.Scraper project

---
+++ Bazirano.Scraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bazirano.Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
