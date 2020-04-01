d90f0802b8421e812f359c6283be4909d9622276
Roko <rokokovac98@gmail.com>
2020-04-01 10:58:42 +0100

    Add InMemoryPostedArticlesRepository tests

---
+++ Bazirano.Tests/Scraper/InMemoryPostedArticlesRepositoryTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Bazirano.Scraper;
+
+namespace Bazirano.Tests.Scraper
+{
+    public class InMemoryPostedArticlesRepositoryTests
+    {
+        [Fact]
+        void AddArticle_AddsArticle()
+        {
+            var repo = new InMemoryPostedArticlesRepository();
+            var article = new Article();
+
+            repo.AddArticle(article);
+
+            Assert.Contains(article, repo.PostedArticles);
+        }
+
+        [Theory]
+        [InlineData(49)]
+        [InlineData(50)]
+        void AddArticle_NumberOfArticlesContained_RemainsAt50Articles(int numberOfArticles)
+        {
+            var repo = new InMemoryPostedArticlesRepository();
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                repo.PostedArticles.Add(new Article());
+            }
+
+            repo.AddArticle(new Article());
+
+            Assert.True(repo.PostedArticles.Count == 50);
+        }
+
+        [Fact]
+        void AddArticle_MoreThan50Articles_RemovesOldestArticle()
+        {
+            var repo = new InMemoryPostedArticlesRepository();
+            var oldestArticle = new Article();
+            repo.PostedArticles.Add(oldestArticle);
+            var secondOldestArticle = new Article();
+            repo.PostedArticles.Add(secondOldestArticle);
+            for (int i = 0; i < 49; i++)
+            {
+                repo.PostedArticles.Add(new Article());
+            }
+
+            repo.AddArticle(new Article());
+            repo.AddArticle(new Article());
+
+            Assert.DoesNotContain(oldestArticle, repo.PostedArticles);
+            Assert.DoesNotContain(secondOldestArticle, repo.PostedArticles);
+        }
+
+        [Theory]
+        [InlineData(true, "test1", "test2", "test3", "test4", "test5")]
+        [InlineData(false, "test1", "test2", "test3", "test4", "test5", "test6")]
+        [InlineData(false, "test1", "test2", "test3", "test4", "test5", "test6", "test7")]
+        void ArticleHasNotBeenPosted_RepoContainsArticleWithMoreThan5MatchingWords_ReturnsFalse(bool expected, params string[] keywords)
+        {
+            var repo = new InMemoryPostedArticlesRepository();
+            repo.PostedArticles.Add(new Article
+            {
+                KeywordsList = keywords
+            });
+            var article = new Article
+            {
+                KeywordsList = new string[] { "test1", "test2", "test3", "test4", "test5", "test6", "test7" }
+            };
+
+            var result = repo.ArticleHasNotBeenPosted(article);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
